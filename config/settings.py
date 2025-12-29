dded1a801f79397da6b68db0d65f8f78f8a11770
Akpere38 <akpereraphael@gmail.com>
2025-12-29 16:52:53 +0100

    Prepare Django backend for deployment

--- config/settings.py
+++ config/settings.py
@@ -24,15 +24,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h-h@p)&cvqee9n&phu(%t4$$7p4ke$@ag2h8o+52i_w0jsv+&#'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ['portfolio.onrender.com']
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
 
 # Application definition
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
