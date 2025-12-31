d0ad548bf00d900f314f8d835a30f5c5e59fc870
Akpere38 <akpereraphael@gmail.com>
2025-12-31 14:26:45 +0100

    Configure S3 media storage correctly

--- config/settings.py
+++ config/settings.py
@@ -146,18 +146,18 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 CORS_ALLOW_ALL_ORIGINS = True
 
 
-# Use S3 for media storage
-AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
-AWS_S3_REGION_NAME = os.getenv("AWS_S3_REGION_NAME", "us-east-1")  # e.g., "us-east-1"
-AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+INSTALLED_APPS += [
+    "cloudinary",
+    "cloudinary_storage",
+]
 
-# Tell Django to use S3 for media files
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 
-# Media URL for uploaded files
-MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
+CLOUDINARY_STORAGE = {
+    "CLOUD_NAME": os.getenv("CLOUDINARY_CLOUD_NAME"),
+    "API_KEY": os.getenv("CLOUDINARY_API_KEY"),
+    "API_SECRET": os.getenv("CLOUDINARY_API_SECRET"),
+}
 
-AWS_QUERYSTRING_AUTH = False
-AWS_DEFAULT_ACL = None
+MEDIA_URL = "/media/"
+
